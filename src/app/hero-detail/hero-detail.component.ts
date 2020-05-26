6c1fd610b8325a2a92bee0e9a532d74809bb9c77
arad12panda <aaradhya.bhushan12@gmail.com>
2020-05-27 00:24:26 +0530

    7 files modified, 4 added service

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService }  from '../hero.service';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -9,9 +14,20 @@ import { Hero } from '../hero';
 export class HeroDetailComponent implements OnInit {
    @Input() panda: Hero;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+  private heroService: HeroService,
+  private location: Location) { }
 
   ngOnInit() {
+    this.getHero();
   }
+  getHero(): void {
+  const id = +this.route.snapshot.paramMap.get('id'); //type casting of strimg to num
+  this.heroService.getHero(id)
+    .subscribe(hero => this.panda = hero);
+}
+goBack(): void {
+  this.location.back();
+}
 
 }
